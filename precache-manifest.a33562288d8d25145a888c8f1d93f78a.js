b02bb29b4a6e112f36dc83b7f2a633975843d01b
sourav789789 <83246577+sourav789789@users.noreply.github.com>
2022-10-05 17:01:17 +0530

    Updates

---
+++ precache-manifest.a33562288d8d25145a888c8f1d93f78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0db65ff96d9427f14b3",
+    "url": "/Textutils/static/css/main.711be562.chunk.css"
+  },
+  {
+    "revision": "a0db65ff96d9427f14b3",
+    "url": "/Textutils/static/js/main.a0db65ff.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Textutils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "47a61aeb0ce2eb63fb5a",
+    "url": "/Textutils/static/js/2.47a61aeb.chunk.js"
+  },
+  {
+    "revision": "9785b7672545a8044e9e",
+    "url": "/Textutils/static/js/runtime~main.9785b767.js"
+  },
+  {
+    "revision": "efb05acb82794c641bde3291d4bc9d02",
+    "url": "/Textutils/index.html"
+  }
+];
